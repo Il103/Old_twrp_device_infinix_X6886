480967e10601ca015f5e25e55b6604fe1fe30315
BERU <beru@recovery.com>
2026-06-30 22:24:25 +0000

    Fix ramdisk, add 120Hz display, bypass decryption password
    - Create missing init.device.rc (fixes boot loop)
    - Add 120Hz/144Hz display support flags
    - Add OF_FORCE_DECRYPT_ON_BOOT for no password prompt
    - Auto MTP/ADB on boot
    - Add 120Hz display properties to system.prop
    - Clean up duplicate crypto flags
    - OrangeFox R12.1 Material UI improvements

--- ofox_X6886.mk
+++ ofox_X6886.mk
@@ -1,3 +1,5 @@
+$(call inherit-product, device/infinix/X6886/device.mk)
+
 PRODUCT_NAME := ofox_X6886
 PRODUCT_DEVICE := X6886
 PRODUCT_BRAND := Infinix
@@ -6,4 +8,30 @@ PRODUCT_MANUFACTURER := INFINIX
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
-PRODUCT_PROPERTY_OVERRIDES +=     ro.ofox.version=R12.1     ro.ofox.build_type=Team-BERU     ro.ofox.maintainer=BERU     ro.ofox.device=X6886     ro.ofox.brand=Infinix     ro.ofox.model=Infinix X6886
+# OrangeFox R12.1 Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ofox.version=R12.1 \
+    ro.ofox.build_type=Team-BERU \
+    ro.ofox.maintainer=BERU \
+    ro.ofox.device=X6886 \
+    ro.ofox.brand=Infinix \
+    ro.ofox.model=Infinix X6886 \
+    ro.ofox.display.size=120Hz@144Hz \
+    ro.ofox.decryption=skip
+
+# OrangeFox R12.1 Features
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ofox.force_decrypt=1 \
+    ro.ofox.no_password_prompt=1 \
+    ro.ofox.format.data=1 \
+    ro.ofox.mtp.auto=1 \
+    ro.ofox.adb.auto=1
+
+# Include OrangeFox addons
+PRODUCT_PACKAGES += \
+    ofox_screen_120hz \
+    ofox_screen_144hz \
+    ofox_decrypt_auto
+
+# Build fingerprint
+BUILD_FINGERPRINT := Infinix/X6886-OP/Infinix-X6886:12/SP1A.210812.016/260325V1508:user/release-keys
