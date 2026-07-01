51c1c080104fb63034e481d3ae8543900cb43b74
BERU <beru@recovery.com>
2026-07-01 00:50:58 +0000

    Fix BoardConfig.mk: correct vendor_boot offsets (base 0x3fff8000, ramdisk 0x26f08000, tags/dtb 0x07c88000), fix recovery.fstab path, fix AndroidProducts.mk LOCAL_DIR

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES :=     /ofox_X6886.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/ofox_X6886.mk
 
 COMMON_LUNCH_CHOICES :=     ofox_X6886-eng     ofox_X6886-userdebug     ofox_X6886-user
